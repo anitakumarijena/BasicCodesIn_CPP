f17351b9200819b04a05b83a8038108b9783e180
Anita kumari jena <64706351+anitakumarijena@users.noreply.github.com>
2021-05-31 22:24:49 +0530

    Structure program

---
+++ struct.cpp
@@ -0,0 +1,29 @@
+""Write a program to store student records in a structure. It should contain the following information:
+Student ID
+Name
+Year
+CGPA""
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef struct student {
+    string id, name;
+    int year;
+    double cgpa;
+};
+
+int main()
+{
+
+    student team[] = {
+        { "GFG1", "Robert", 3, 8.67 },
+        { "GFG2", "Chris", 2, 9.13 },
+        { "GFG3", "Mark", 4, 8.43 },
+    };
+
+    for (int i = 0; i < 3; i++)
+        cout << "ID: " << team[i].id << ", Name: " << team[i].name << ", Year: " << team[i].year << ", CGPA: " << team[i].cgpa << endl;
+
+    return 0;
+}
